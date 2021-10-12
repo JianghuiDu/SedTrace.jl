935faee1b6c54f75f725f3f4ef83c1998c559199
Jianghui Du <31140772+JianghuiDu@users.noreply.github.com>
2021-10-12 23:40:31 +0200

    Update SedTrace.jl

--- src/SedTrace.jl
+++ src/SedTrace.jl
@@ -1,5 +1,25 @@
 module SedTrace
 
-# Write your package code here.
+using SciMLBase
+using Sundials
+using DiffEqCallbacks
+using PreallocationTools
+using DiffEqOperators
+# using Preconditioners
+using ILUZero
+using FiniteDiff
+using ForwardDiff
+using SpecialFunctions
+using LinearAlgebra
+using SparseArrays
+using SparseDiffTools
+using BandedMatrices
+using FastBroadcast
+using DataFramesMeta
+using DataFrames
+using XLSX
+using Plots
+using StatsPlots
+using JuliaFormatter
 
 end
