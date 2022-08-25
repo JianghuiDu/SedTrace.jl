192eb5ced60a00fb402db5378e43cf76a4848490
Jianghui Du <31140772+JianghuiDu@users.noreply.github.com>
2022-08-25 10:22:36 +0200

    update generate_struct
    new struct type to be compatible with PreallocationTools 0.4

--- src/generate_struct.jl
+++ src/generate_struct.jl
@@ -1,22 +1,29 @@
 function struct_code(cache)
     struct_str = String[]
-    push!(struct_str, "mutable struct Reactran{T,chunk_size}")
+    # push!(struct_str, "mutable struct Reactran{T,chunk_size}")
+    push!(struct_str, "mutable struct Reactran{T}")
     for i in cache
+        # push!(struct_str, i * "::PreallocationTools.DiffCache{
+        #    Array{T,1},
+        #    Array{ForwardDiff.Dual{nothing,T,chunk_size},1},
+        #    }")
         push!(struct_str, i * "::PreallocationTools.DiffCache{
-           Array{T,1},
-           Array{ForwardDiff.Dual{nothing,T,chunk_size},1},
-       }")
-    end
+            Array{T,1},
+            Array{T,1},
+            }")
+     end
     push!(struct_str, "end")
 
 
     push!(struct_str, "")
     push!(
         struct_str,
-        "function init(u0::Array{T,1},Ngrid::Int, ::Val{chunk_size}) where {T,chunk_size}",
+        # "function init(u0::Array{T,1},Ngrid::Int, ::Val{chunk_size}) where {T,chunk_size}",
+        "function init(u0::Array{T,1},Ngrid::Int, chunk_size::Int) where {T}",
     )
     for i in cache
-        push!(struct_str, i * "= PreallocationTools.dualcache(zeros(T,Ngrid), Val{chunk_size})")
+        # push!(struct_str, i * "= PreallocationTools.dualcache(zeros(T,Ngrid), Val{chunk_size})")
+        push!(struct_str, i * "= PreallocationTools.dualcache(zeros(T,Ngrid), chunk_size)")
     end
     push!(struct_str, "")
 
