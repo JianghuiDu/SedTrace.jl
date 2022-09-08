57b0d0d385b96b6b92dcba3ff6be0712e086243f
Jianghui Du <31140772+JianghuiDu@users.noreply.github.com>
2022-09-08 20:48:51 +0200

    Update generate_jacobian.jl
    New adsorption and speciation interface, allow passing parameter struct

--- src/generate_jacobian.jl
+++ src/generate_jacobian.jl
@@ -4,8 +4,9 @@ function generate_jacobian(
     jp::Union{BandedMatrix{T},SparseMatrixCSC{T,Int}},
     # C0::Vector{T},
     chunk_size::Int,
+    parm
 ) where {T}
-    f_ = (du, u) -> f(du, u, nothing, 0.0)
+    f_ = (du, u) -> f(du, u, parm, 0.0)
     colors = SparseDiffTools.matrix_colors(jp)
     Nmat = size(jp,1)
     utmp = zeros(T,Nmat)
@@ -15,28 +16,30 @@ function generate_jacobian(
     return (J, u, p, t) -> SparseDiffTools.forwarddiff_color_jacobian!(J, f_, u, jac_cache)
 end
 
-# FiniteDiff FD with sparsity pattern
-function generate_jacobian(
-    f,
-    jp::Union{BandedMatrix{T},SparseMatrixCSC{T,Int}},
-) where {T}
-    f_ = (y, x) -> f(y, x, nothing, 0.0)
-    colors = SparseDiffTools.matrix_colors(jp)
-    Nmat = size(jp,1)
-    utmp = zeros(T,Nmat)
-    dutmp = zeros(T,Nmat)
-    dutmp1 = zeros(T,Nmat)
-    jac_cache = FiniteDiff.JacobianCache(utmp,dutmp,dutmp1,Val{:complex}; colorvec = colors, sparsity = jp)
-    return (J, u, p, t) -> FiniteDiff.finite_difference_jacobian!(J, f_, u, jac_cache,absstep=1e-8)
-end
+# # FiniteDiff FD with sparsity pattern
+# function generate_jacobian(
+#     f,
+#     jp::Union{BandedMatrix{T},SparseMatrixCSC{T,Int}},
+#     parm
+# ) where {T}
+#     f_ = (y, x) -> f(y, x, parm, 0.0)
+#     colors = SparseDiffTools.matrix_colors(jp)
+#     Nmat = size(jp,1)
+#     utmp = zeros(T,Nmat)
+#     dutmp = zeros(T,Nmat)
+#     dutmp1 = zeros(T,Nmat)
+#     jac_cache = FiniteDiff.JacobianCache(utmp,dutmp,dutmp1,Val{:complex}; colorvec = colors, sparsity = jp)
+#     return (J, u, p, t) -> FiniteDiff.finite_difference_jacobian!(J, f_, u, jac_cache,absstep=1e-8)
+# end
 
 # ForwardDiff AD without sparsity pattern
 function generate_jacobian(
     f,
     jp::Matrix{T},
-    chunk_size::Int
+    chunk_size::Int,
+    parm
 ) where {T}
-    f_ = (y, x) -> f(y, x, nothing, 0.0)
+    f_ = (y, x) -> f(y, x, parm, 0.0)
     Nmat = size(jp,1)
     utmp = zeros(T,Nmat)
     dutmp = zeros(T,Nmat)
