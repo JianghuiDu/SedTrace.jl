cb700758fb783e7975d4fbd07806da431a27552b
Jianghui Du <31140772+JianghuiDu@users.noreply.github.com>
2023-03-02 11:03:42 +0100

    add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,63 @@ using SedTrace
 using Test
 
 @testset "SedTrace.jl" begin
-    # Write your tests here.
+    modeldirectory = (@__DIR__)*"\\"
+    modelfile = "model_config.ammonia.xlsx" 
+    modelname = "ammonia"
+    
+    modelconfig = ModelConfig(
+        modeldirectory,
+        modelfile,
+        modelname,
+    )
+    
+    @time generate_parameter_template(modelconfig)
+    
+    @time generate_code(modelconfig)
+    
+    IncludeFiles(modelconfig)
+    
+    # initial values
+    C0 = Param.C0;
+    # initalize parameters
+    parm = Param.ParamStruct();
+    # initialize cache and ODE function
+    OdeFun = Cache.init(C0, parm.Ngrid);
+    # initialize Jacobian 
+    JacPrototype = JacType(Param.IDdict);
+    
+    
+    TestOdeFun(OdeFun,C0,parm)
+    TestJacobian(JacPrototype,OdeFun,parm)
+    BenchmarkReactran(OdeFun,C0,parm)
+    BenchmarkJacobian(JacPrototype,OdeFun,parm)
+    BenchmarkPreconditioner(JacPrototype,OdeFun,parm,:ILU0)
+    
+    # configure the solver
+    solverconfig = SolverConfig(:GMRES, :ILU0, 2)
+    
+    
+    
+    solutionconfig = SolutionConfig(
+        C0,
+        (0.0, 1.0),
+        reltol = 1e-6,
+        abstol = 1e-12,
+        saveat = 100.0,
+        callback = TerminateSteadyState(1e-16, 1e-6),
+    );
+    
+    
+    
+    solution = modelrun(OdeFun, parm, JacPrototype, solverconfig, solutionconfig);
+    
+    gr(; size = (400, 800))
+    
+    generate_output(
+        modelconfig,
+        solution,
+        site = "analytical",
+        showplt = true,
+    )
+    
 end
